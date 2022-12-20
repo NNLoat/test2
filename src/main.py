24580a38cbdc3d66a0c72a36a32f27a1ce59e74c
NNLoat <83104226+NNLoat@users.noreply.github.com>
2022-12-20 14:18:13 +0700

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+def main():
+    print "hello" #intentionally wrong
+    L1 = [1,2,3]
+    print (L1[3])
+    
+    D1={'1':"aa", '2':"bb", '3':"cc"}
+    print(D1['4'])
+    
+    
+    print('2'+2)
